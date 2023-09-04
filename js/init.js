7e8a066c98be4b8d394a5cd058db0917354fe837
copofo <121464011+copofo@users.noreply.github.com>
2023-09-04 03:26:09 -0300

    9

---
+++ js/init.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAQHTo14OgEr8q56qLsIZC85CXIjcatYbM",
+    authDomain: "trio-ribeirao.firebaseapp.com",
+    projectId: "trio-ribeirao",
+    storageBucket: "trio-ribeirao.appspot.com",
+    messagingSenderId: "283549252712",
+    appId: "1:283549252712:web:6c73797dc8fa9320ef8fdd",
+    measurementId: "G-EBLKLCM893"
+};
+
+
+firebase.initializeApp(firebaseConfig);
--- js/login.js
+++ js/login.js
@@ -4,3 +4,11 @@ const f = {
     btn_login: () => document.getElementById('btn_login')
 }
 
+f.btn_login().addEventListener('click')
+firebase.auth().signInWithEmailAndPassword(f.email.value, f.password.value)
+.then(user => {
+    alert("LOGIN REALIZADO COM SUCESSO.")
+})
+.catch(erro =>{
+    console.log(erro)
+})
