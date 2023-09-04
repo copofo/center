c2c88a7b1bbdf78913a3e5d6a25a13820667726a
copofo <121464011+copofo@users.noreply.github.com>
2023-09-04 01:46:22 -0300

    init

---
+++ js/login.js
@@ -0,0 +1,6 @@
+const f = {
+    email: () => document.getElementById('email'),
+    password: () => document.getElementById('password'),
+    btn_login: () => document.getElementById('btn_login')
+}
+
